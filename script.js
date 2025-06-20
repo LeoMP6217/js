2fbfcca0b69f56b2577708eaa416986ae7600aa2
LeoMP6217 <lmorales6217@gmail.com>
2025-06-20 11:18:57 -0700

    IDK what im doing

---
+++ script.js
@@ -0,0 +1,11 @@
+function setup () //called once when the program starts
+{
+    createCanvas(400,400);
+    background(55,232,233);
+    circle(100,100,100)
+}
+
+function draws () //this runs in a loop to update the display
+{
+
+}
